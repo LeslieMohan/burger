2bdb029a3edc5e2ff9461019999cbd474a3bf8c4
LeslieMohan <donnalesliedesign@gmail.com>
2018-07-24 20:58:06 -0400

    latest updates

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# burger
+# Burger
+Eat-Da-Burger! is a restaurant app that lets users input the names of burgers they woud like to eat.  Whenever a user submits a burger's name, your app will display the burger on the left side of the page -- waiting to be devoured.  Each burger in the waiting area also has a Devour it! button.  When the user clicks it, the burger will move to the right side of the page.
+Your app will store every burger in a database, whether devoured or not.
+
+## Check it out here!
+
+https://enthousiaste-chocolatine-75598.herokuapp.com/
+
+
+### Created using
+- MySQL
+- Node
+- Express
+- Handlebars
+- ORM
+ 
--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
 
 
 var PORT = process.env.PORT || 3000;
 
 var app = express();
 
+//serve static content
 app.use(express.static("public"));
+
+//parse application
 app.use(bodyParser.urlencoded({ extended: true }));
+//parse application/json
 app.use(bodyParser.json());
 
-
+//set handlebars
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller");
+//import routes and have access to them
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
-
+//start server and have it listen to client request
 app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
 });
